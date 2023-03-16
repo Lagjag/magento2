daf17dfe694655a581b35cd0680ec7333d7c75c0
Lagjag <lualamillo@gmail.com>
2023-03-16 11:00:16 +0100

    fix

--- app/code/Custom/ModuloBasico/Block/Custom.php
+++ app/code/Custom/ModuloBasico/Block/Custom.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Custom\ModuloBasico\Block\Custom;
+namespace Custom\ModuloBasico\Block;
 
 use Magento\Framework\View\Element\Template;
 
 class Custom extends Template
 {
     public function __construct(
-        \Magento\Framework\View\Template\Context $context,
+        \Magento\Framework\View\Element\Template\Context $context,
         array $data = []
     ){
         parent::__construct($context, $data);
