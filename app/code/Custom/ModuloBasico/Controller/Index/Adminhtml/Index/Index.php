8f95207844ed51a50a39112d70817b8ff114c972
Lagjag <lualamillo@gmail.com>
2023-03-20 09:54:29 +0100

    primer modulo admin

---
+++ app/code/Custom/ModuloBasico/Controller/Index/Adminhtml/Index/Index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Custom\ModuloBasico\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ){
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+
+    public function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Custom_ModuloBasico::index');
+    }
+}
